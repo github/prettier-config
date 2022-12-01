8fd1960c0db681522c815f7afb637ae92001959f
Keith Cirkel <keithamus@users.noreply.github.com>
2022-12-01 10:24:22 +0000

    add tests

---
+++ examples/one.ts
@@ -0,0 +1,23 @@
+const obj = {
+  one: 1,
+  two: 2,
+  three: 3,
+}
+
+;('this config avoids semicolons at the end of statements and uses single quotes for strings')
+
+async function heresAFunctionWhichSpansMulitpleLinesAnd(
+  that,
+  includes,
+  many,
+  args,
+  to,
+  demonstrait,
+  trailing,
+  func,
+  commas
+) {
+  return obj
+}
+
+const arrowFunctions = avoidParens => 1
